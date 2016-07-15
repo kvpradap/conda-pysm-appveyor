2b975e274b0d2596aa94cfdfd6164c497e3f7b56
kvpradap <pradap@cs.wisc.edu>
2016-07-15 18:54:49 -0500

    [INI] Added first set of files.

---
+++ setup.py
@@ -0,0 +1,25 @@
+import os
+from setuptools import setup, Extension
+from Cython.Build import cythonize
+
+#####################################
+VERSION = "1.0"
+ISRELEASED = False
+__version__ = VERSION
+#####################################
+
+
+extension_module = Extension(
+    'pyappveyordemo.extension',
+     sources=['pyappveyordemo/extension.pyx']
+)
+
+
+setup(
+    name = 'python-appveyor-demo',
+    version = VERSION,
+    description = 'This is a demo package with a compiled C extension.',
+    ext_modules = cythonize([extension_module]),
+    zip_safe=False,
+    packages=['pyappveyordemo', 'pyappveyordemo.tests'],
+)
