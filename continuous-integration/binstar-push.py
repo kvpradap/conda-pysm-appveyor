0428098c35e3b25412c0cb44021f94aae54641ad
kvpradap <pradap@cs.wisc.edu>
2016-07-15 19:39:47 -0500

    [FIX] Removed binstar related stuff

--- continuous-integration/binstar-push.py
+++ continuous-integration/binstar-push.py
@@ -3,11 +3,11 @@ import glob
 import subprocess
 import traceback
 
-token = os.environ['BINSTAR_TOKEN']
-cmd = ['binstar', '-t', token, 'upload', '--force']
-cmd.extend(glob.glob('*.tar.bz2'))
-try:
-    subprocess.check_call(cmd)
-except subprocess.CalledProcessError:
-    traceback.print_exc()
+#token = os.environ['BINSTAR_TOKEN']
+#cmd = ['binstar', '-t', token, 'upload', '--force']
+#cmd.extend(glob.glob('*.tar.bz2'))
+#try:
+#    subprocess.check_call(cmd)
+#except subprocess.CalledProcessError:
+#    traceback.print_exc()
 
